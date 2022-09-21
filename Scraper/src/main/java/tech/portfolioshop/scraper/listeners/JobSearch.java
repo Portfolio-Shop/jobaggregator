cd949299ff1e11920d226d5a1b9b952243883457
pollygen <sudip.wfhintellify@gmail.com>
2022-09-21 20:21:44 +0530

    add listener to scraper

---
+++ Scraper/src/main/java/tech/portfolioshop/scraper/listeners/JobSearch.java
@@ -0,0 +1,19 @@
+package tech.portfolioshop.scraper.listeners;
+
+import org.jobaggregator.kafka.payload.JobSearchTriggered;
+import org.jobaggregator.kafka.payload.UserDeleted;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@EnableKafka
+@Component
+public class JobSearch {
+
+    @KafkaListener(topics = "JOB_SEARCH_TRIGGERED", groupId = "${spring.application.name}")
+    public void jobSearchTriggered(String message) {
+        JobSearchTriggered jobSearchTriggered = new JobSearchTriggered(null, null);
+        jobSearchTriggered.deserialize(message);
+        System.out.println(message);
+    }
+}
