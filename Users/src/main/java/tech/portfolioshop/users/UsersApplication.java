34ea5a44c3f9ed1bd6cb89c7c5f150ae159542cb
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2022-09-25 13:08:12 +0000

    update docker configurations

--- Users/src/main/java/tech/portfolioshop/users/UsersApplication.java
+++ Users/src/main/java/tech/portfolioshop/users/UsersApplication.java
@@ -1,23 +1,13 @@
 package tech.portfolioshop.users;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class UsersApplication {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
-
-    @Autowired
-    public UsersApplication(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
     public static void main(String[] args) {
         SpringApplication.run(UsersApplication.class, args);
     }
@@ -25,9 +15,5 @@ public class UsersApplication {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-    @EventListener(ApplicationReadyEvent.class)
-    public void connectToKafka() {
-        kafkaTemplate.send("connect", "connect");
     }
 }
