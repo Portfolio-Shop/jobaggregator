20672e5d4ae9a4d54dcfc03c708c53c71a42a5e6
sonishsinghal <sonishsinghal@gmail.com>
2022-10-27 01:15:33 +0530

    Create next template

---
+++ client/src/pages/signin.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Signin: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Signin</h1>
+    </div>
+  )
+}
+
+export default Signin
