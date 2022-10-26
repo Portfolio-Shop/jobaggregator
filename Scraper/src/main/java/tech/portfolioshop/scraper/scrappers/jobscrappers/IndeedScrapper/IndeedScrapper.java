86eb9b986107007e64e44f1ba2d44e558c3d1e7c
sonishsinghal <sonishsinghal@gmail.com>
2022-10-26 21:49:14 +0530

    Add indeed

--- Scraper/src/main/java/tech/portfolioshop/scraper/scrappers/jobscrappers/IndeedScrapper/IndeedScrapper.java
+++ Scraper/src/main/java/tech/portfolioshop/scraper/scrappers/jobscrappers/IndeedScrapper/IndeedScrapper.java
@@ -19,12 +19,31 @@ public class IndeedScrapper extends SeleniumScrapper {
     @Override
     public String generateUrl() {
         //TODO: Create Generate Url Method for Scrapper
-        return null;
+        String seperatedQuery = "", seperatedLocation = "";
+        for (String word : query.split(" ")) seperatedQuery += word + "-";
+        for (String word : location.split(" ")) seperatedLocation += word + "-";
+        seperatedQuery = seperatedQuery.trim();
+        seperatedLocation = seperatedLocation.trim();
+        if (seperatedQuery.length() > 0) seperatedQuery = seperatedQuery.substring(0, seperatedQuery.length() - 1);
+        if (seperatedLocation.length() > 0)
+            seperatedLocation = seperatedLocation.substring(0, seperatedLocation.length() - 1);
+        return website.getBaseUrl() + "jobs";
     }
 
     @Override
     public List<JobModel> scrape() {
         //TODO: Create Scrapper for Indeed
+
+        List<JobModel> jobModels;
+        try {
+            webDriver.get(generateUrl());
+            Thread.sleep(7000);
+            websiteTabHandle = webDriver.getWindowHandle();
+
+        }
+        catch (){
+
+        }
         return null;
     }
 }
